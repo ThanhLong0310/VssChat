efb8b7d409d437c910c227eb2fcf8faf296a81e3
ThanhLong0310 <long.lt0310@gmail.com>
2026-03-04 10:00:26 +0700

    complete project

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -8,13 +8,10 @@ import { Router } from '@angular/router';
 })
 export class ForgotPasswordComponent {
   email: string = '';
-
   constructor(private router: Router) { }
-
   onSubmit() {
     if (this.email) {
       console.log('Yêu cầu tìm tài khoản cho:', this.email);
-      // Chuyển sang bước 2: Verify OTP
       this.router.navigate(['/verify-otp']);
     }
   }
